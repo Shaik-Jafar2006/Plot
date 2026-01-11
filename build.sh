5b63a663ebc94f7b395dcc2d509f5538516281f0
Shaik-Jafar2006 <shaikjafar2010@gmail.com>
2026-01-11 16:58:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/env bash
-
-# exit on error
-set -e
-
-# install dependencies
+#!/bin/bash
 pip install -r requirements.txt
-
-# run migrations
 python manage.py migrate
-
-# collect static files
 python manage.py collectstatic --noinput
